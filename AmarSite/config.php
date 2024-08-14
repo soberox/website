a7a3ec370f6d9926c1418e601ca63905f2143597
brawl7787 <79094576+soberox@users.noreply.github.com>
2024-08-14 11:19:08 -0400

    Delete AmarSite directory

--- AmarSite/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-    define('servername', 'localhost');
-    define('username', 'root');
-    define('password', 'kU&p7W^Yt>5KHJGn-Q#Zm@');
-    define('database', 'form');
-    
