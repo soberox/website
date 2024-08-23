a652973575b181edbfd1b026718905b1a48903cc
brawl7787 <79094576+soberox@users.noreply.github.com>
2024-08-22 21:36:34 -0400

    Delete guayan site/AmarSite directory

--- guayan site/AmarSite/js/login.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector("#start_login").addEventListener("click",function(){
-    document.querySelector(".popup_login").classList.add("active");
-});
-
-document.querySelector("#close_log").addEventListener("click",function(){
-    document.querySelector(".popup_login").classList.remove("active");
-});
