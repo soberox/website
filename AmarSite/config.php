cc85cbe19f3a152fed7c0d2ca2ac73912d9106f9
brawl7787 <79094576+soberox@users.noreply.github.com>
2024-08-22 21:32:30 -0400

    Delete AmarSite directory

--- AmarSite/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-    define('servername', '192.168.254.81');
-    define('username', 'root');
-    define('password', 'kU&p7W^Yt>5KHJGn-Q#Zm@');
-    define('database', 'form');
-    
-    
--- AmarSite/form(office).js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector("#add_note").addEventListener("click",function(){
-    document.querySelector(".popup_note").classList.add("active");
-});
-
-document.querySelector("#close_note").addEventListener("click",function(){
-    document.querySelector(".popup_note").classList.remove("active");
-});
