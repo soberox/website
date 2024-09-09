5a7471302c213910ce9170433285073738901827
brawl7787 <79094576+soberox@users.noreply.github.com>
2024-09-08 20:26:47 -0400

    Delete guayan site directory

--- guayan site/login/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-    define('servername', 'forms.cdgs8omyu4nt.us-east-1.rds.amazonaws.com');
-    define('username', 'admin');
-    define('password', 'GzpRxwg9p24VLoGjUzxA');
-    define('database', 'form');
-    
-    
